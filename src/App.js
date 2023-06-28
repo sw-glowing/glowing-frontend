a70c2d412d762ad05cea802e0d41e6f12984fbcd
SeiwonPark <psw7347@gmail.com>
2023-06-29 07:02:28 +0900

    chore: formatted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import React, { Routes, Route } from 'react-router-dom'
+
+import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />;
     </Routes>
-  );
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import App from "./App";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import React from "react";
-import ReactDOM from "react-dom/clients";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </BrowserRouter>
-);
+)
 
-reportWebVitals();
+reportWebVitals()
