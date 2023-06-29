921b85babb64a66e07045375b8c0e8cd8b84d4c5
yaehee <yaehee@kakao.com>
2023-06-29 11:56:52 +0900

    다크모드 토글 구현 완

--- src/component/layout/IngameLayout.js
+++ src/component/layout/IngameLayout.js
@@ -3,22 +3,40 @@ import Drawer from 'react-modern-drawer'
 //import styles 👇
 import 'react-modern-drawer/dist/index.css'
 
+import styled, { ThemeProvider } from 'styled-components'
+
+import { dark, light } from '../../style/theme'
+import { useTheme } from '../../style/useTheme'
 import IngameNavbar from '../IngameNavbar'
 
+const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${(props) => props.theme.colors.bgColor};
+  color: ${(props) => props.theme.colors.textColor};
+`
+
 const IngameLayout = ({ children }) => {
+  const [themeMode, toggleTheme] = useTheme() // hook 함수 하용
+  const theme = themeMode === 'light' ? light : dark
   const [isOpen, setIsOpen] = React.useState(false)
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState)
   }
 
   return (
-    <>
-      <IngameNavbar onMenu={toggleDrawer} />
-      <>{children}</>
-      <Drawer open={isOpen} onClose={toggleDrawer} direction="right" className="bla bla bla">
-        <div>Hello World</div>
-      </Drawer>
-    </>
+    <ThemeProvider theme={theme}>
+      <Main>
+        <IngameNavbar onMenu={toggleDrawer} />
+        <>{children}</>
+        <Drawer open={isOpen} onClose={toggleDrawer} direction="right" className="bla bla bla">
+          <button
+            title={theme === 'light' ? '일반모드로 테마 변경하기' : '다크모드로 테마 변경하기'}
+            onClick={toggleTheme}
+          />
+        </Drawer>
+      </Main>
+    </ThemeProvider>
   )
 }
 
---
+++ src/style/theme.js
@@ -0,0 +1,13 @@
+export const dark = {
+  colors: {
+    textColor: '#121212',
+    bgColor: '#b8b8b8',
+  },
+}
+
+export const light = {
+  colors: {
+    textColor: '#b8b8b8',
+    bgColor: '#121212',
+  },
+}
