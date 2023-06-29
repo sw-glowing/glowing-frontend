d87a946ea511d85890c49ceb7e6b3c5dc89faddd
yaehee <yaehee@kakao.com>
2023-06-29 09:25:15 +0900

    lint

--- src/page/LearnPage.js
+++ src/page/LearnPage.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
+
 function LearnPage() {
   return (
     <>
-      
       <h2>단어</h2>
       <p>뜻풀이</p>
     </>
-  )
+  );
 }
 
-export default LearnPage
+export default LearnPage;
