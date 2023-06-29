9e4e88450f87d44b0af666bbc19710a7a4c2bf12
yaehee <yaehee@kakao.com>
2023-06-29 17:23:47 +0900

    edit font size volume

--- src/component/layout/IngameLayout.js
+++ src/component/layout/IngameLayout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Drawer from 'react-modern-drawer'
 //import styles 👇
 import 'react-modern-drawer/dist/index.css'
@@ -15,33 +15,49 @@ const Main = styled.div`
   height: 100vh;
   background-color: ${(props) => props.theme.colors.bgColor};
   color: ${(props) => props.theme.colors.textColor};
+  font-size: ${(props) => props.fontSize}px;
+`
+const SubmitBtn = styled.div`
+  font-size: 1em;
 `
 const IngameLayout = ({ children }) => {
   const [themeMode, allThemes, setMode] = useTheme()
   const themeKey = ['dark', 'light', 'bubblegum']
   const theme = allThemes[themeKey.indexOf(themeMode)]
+  const [volume, setVolume] = useState(1)
   const [isOpen, setIsOpen] = React.useState(false)
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState)
   }
-  console.log(theme)
   return (
-    <ThemeProvider theme={theme}>
-      <Main>
-        <IngameNavbar onMenu={toggleDrawer} />
+    <ThemeProvider theme={theme} fontSize={0}>
+      <IngameNavbar onMenu={toggleDrawer} />
+      <Main fontSize={parseInt(volume) + 12}>
         <>{children}</>
-        <Drawer open={isOpen} onClose={toggleDrawer} direction="right" className="bla bla bla">
-          {allThemes.map((t, i) => (
-            <ThemeButton
-              key={i}
-              themeColor={t}
-              onClick={() => {
-                setMode(themeKey[i])
-              }}
-            />
-          ))}
-        </Drawer>
       </Main>
+      <Drawer open={isOpen} onClose={toggleDrawer} direction="right" className="bla bla bla">
+        {allThemes.map((t, i) => (
+          <ThemeButton
+            key={i}
+            themeColor={t}
+            onClick={() => {
+              setMode(themeKey[i])
+            }}
+          />
+        ))}
+        <input
+          type="range"
+          min={0}
+          max={40}
+          color="gray"
+          step={2}
+          value={volume}
+          onChange={(event) => {
+            setVolume(event.target.valueAsNumber)
+          }}
+        />
+        <div style={{ fontSize: 10, border: '1px solid pink', borderRadius: 5, padding: 5 }}>{volume + 12} px</div>
+      </Drawer>
     </ThemeProvider>
   )
 }
