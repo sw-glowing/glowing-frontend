1242af9ab54f06a92f7fedec1ca51ac31639432f
SeiwonPark <63793178+SeiwonPark@users.noreply.github.com>
2023-06-30 11:15:05 +0900

    Revert "feat: final code update for sw-2023"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react'
-
-import { Routes, Route } from 'react-router-dom'
-
-import './App.css'
-//import HomePage from './page/HomePage'
-import LearnPage from './page/LearnPage'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LearnPage />} />;
-    </Routes>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
